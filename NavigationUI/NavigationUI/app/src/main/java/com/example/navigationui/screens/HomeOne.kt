30ca051a562e289ab677dbf0690a53e02994f1ea
wakolivotes <votesdw@yahoo.com>
2022-11-19 10:05:32 +0300

    Fix NavigationUI

--- NavigationUI/NavigationUI/app/src/main/java/com/example/navigationui/screens/HomeOne.kt
+++ NavigationUI/NavigationUI/app/src/main/java/com/example/navigationui/screens/HomeOne.kt
@@ -1,9 +1,13 @@
 package com.example.navigationui.screens
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.text.ClickableText
 import androidx.compose.material.OutlinedTextField
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.AnnotatedString
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
@@ -14,19 +18,30 @@ import com.example.navigationui.Routes
 
 @Composable
 fun HomeOne( navController: NavHostController ) {
-Column() {
+Column(
+    modifier = Modifier.fillMaxSize(),
+    horizontalAlignment = Alignment.CenterHorizontally,
+    verticalArrangement = Arrangement.Center
+) {
     OutlinedTextField(
-        value = "Home One: We are at the Home Dashboard",
+        value = "Home 1 Screen",
         onValueChange = {""} )
     
+    // To Home 2
     ClickableText(
-        text = AnnotatedString("Home Two"),
+        text = AnnotatedString("Click to Go to Home 2"),
         onClick = { navController.navigate(Routes.HomeTwo.route)},
         style = TextStyle(
             fontSize = 14.sp,
             fontFamily = FontFamily.Default
         )
     )
+    // To Home 3
+    ClickableText(
+        text = AnnotatedString("Click to Go to Home 3") ,
+        onClick =  { navController.navigate(Routes.HomeThree.route) }
+    )
+
     
 }
 }
--- NavigationUI/NavigationUI/app/src/main/java/com/example/navigationui/screens/HomeTwo.kt
+++ NavigationUI/NavigationUI/app/src/main/java/com/example/navigationui/screens/HomeTwo.kt
@@ -1,23 +1,38 @@
 package com.example.navigationui.screens
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.text.ClickableText
 import androidx.compose.material.OutlinedTextField
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.AnnotatedString
 import androidx.navigation.NavHostController
 import com.example.navigationui.Routes
 
 @Composable
 fun HomeTwo (navController: NavHostController){
-Column() {
+Column(
+    modifier = Modifier.fillMaxSize(),
+    horizontalAlignment = Alignment.CenterHorizontally,
+    verticalArrangement = Arrangement.Center
+
+) {
     OutlinedTextField(
-        value = "Home Two: Dashboard Two",
+        value = "Home 2 Screen",
         onValueChange = {""}
     )
+    // To Home 3
     ClickableText(
-        text = AnnotatedString("Go to Screen Three"),
+        text = AnnotatedString("Click to Go to Home 3"),
         onClick = { navController.navigate(Routes.HomeThree.route)}
+    )
+    // To Home 1
+    ClickableText(
+        text = AnnotatedString("Click to Go to Home 1") ,
+        onClick =  { navController.navigate(Routes.HomeOne.route) }
     )
 
 }

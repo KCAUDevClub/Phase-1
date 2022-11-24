f1c622fb2f2a952ee560ede44894f2f7a1e8c550
wakolivotes <votesdw@yahoo.com>
2022-11-25 02:04:08 +0300

    UI Compose + XML

---
+++ UIImplementationCompose/app/src/main/java/com/example/uicompose/FunMainNav.kt
@@ -0,0 +1,14 @@
+package com.example.uicompose
+
+import androidx.compose.material.OutlinedButton
+import androidx.compose.runtime.Composable
+
+@Composable
+fun FunMain(){
+
+OutlinedButton(onClick = { /*TODO*/ }) {
+    
+}
+
+
+}
